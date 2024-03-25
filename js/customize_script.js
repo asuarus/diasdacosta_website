a9643f80d0b955602b6a7350ec44a7382022652b
asuarus <daigenb@bu.edu>
2024-03-26 10:59:39 +1100

    js update and file sort

---
+++ js/customize_script.js
@@ -0,0 +1,6 @@
+const calculations = require('./calculations');
+
+// Now you can use the functions from calculations.js
+// For example:
+const result = calculations.add(2, 3);
+console.log(result);
